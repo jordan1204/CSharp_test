57ff1c0f8d909ee3365fc657093090a5eac6349b
Yun-Ching Lee <a0933041015@gmail.com>
2020-07-06 12:02:17 +0800

    Add files via upload

---
+++ oracle_db_test.cs
@@ -0,0 +1,38 @@
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "User Id=sajet;Password=tech;Data Source=192.168.1.103:1521/ORCL4;";
+
+            var connection = new OracleConnection();
+            connection.ConnectionString = connectionString;
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = @"
+SELECT
+    name
+FROM EMS_ACCOUNT
+";
+
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+
+            connection.Close();
+            Console.WriteLine("Press 'Enter' to continue");
+            Console.ReadLine();
+        }
+    }
+}
